7e1280d28a7756dc42c0df1bb7e6613912c88a62
Matias-Semere <MatiasSolomon21@gmail.com>
2025-10-08 16:25:00 +0200

    test

--- src/lab3/model/App.java
+++ src/lab3/model/App.java
@@ -1,7 +1,5 @@
 package lab3.model;
 
-import java.time.LocalDateTime;
-
 import javax.swing.SwingUtilities;
 import lab3.clock.WeekAlarmClock;
 import lab3.controller.*;
@@ -10,9 +8,6 @@ import lab3.view.*;
 public class App {
     public static void main(String[] args) {
         WeekAlarmClock clock = new WeekAlarmClock();
-        LocalDateTime real = LocalDateTime.now();
-
-
 
         MainController mainc = new MainController(clock);
         AlarmController alarmc = new AlarmController(clock);
